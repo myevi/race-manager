657d390dcff9600be7e452fc4c3ba07bb9b48263
myevi <yevishev.m@gmail.com>
2024-08-05 07:59:10 +0300

    fix test

--- main_test.go
+++ main_test.go
@@ -7,7 +7,7 @@ import (
 func TestMain(t *testing.T) {
 	res, err := parsePDF("silverstone2024.pdf")
 	if err != nil {
-		t.Log("some err: %w", err)
+		t.Log("some err", err.Error())
 		t.Fail()
 	}
 
